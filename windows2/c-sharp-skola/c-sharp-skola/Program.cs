64f45ae285983f6ca345da72c1cd101119c5e584
Markus Simonsen <markus.simonsen@learnet.se>
2020-09-11 10:27:19 +0200

    Did school work

---
+++ windows2/c-sharp-skola/c-sharp-skola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+
+            for(var i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Namn " +  i + ":");
+                names[i - 1] = Console.ReadLine();
+            }
+
+            Array.Reverse(names);
+
+            var jsonString = JsonSerializer.Serialize(names);
+
+            Console.WriteLine(jsonString);
+
+        }
+    }
+}
